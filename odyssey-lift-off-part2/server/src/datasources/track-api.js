fe7bc580575346440d4b4b0fb13b32f897c38060
Bramandia Gilanglaksana Adam <gilangbram@gmail.com>
2021-11-30 18:41:36 +0700

    :zap: implementing RestDataSource

---
+++ odyssey-lift-off-part2/server/src/datasources/track-api.js
@@ -0,0 +1,20 @@
+const { RESTDataSource } = require('apollo-datasource-rest');
+
+class TrackAPI extends RestDataSource {
+  constructor() {
+    super();
+    this.baseURL = 'https://odyssey-lift-off-rest-api.herokuapp.com/';
+  }
+
+  getTracksForHome() {
+    // GET request to the tracks endpoint
+    return this.get('tracks');
+  }
+
+  getAuthor(authorId) {
+    // GET call to the /author/:id endpoint
+    return this.get(`author/${authorId}`);
+  }
+}
+
+module.exports = TrackAPI;
