e746e2b41f79f2ea97423a67663b9008d27c5fd8
Bramandia Gilanglaksana Adam <gilangbram@gmail.com>
2021-12-05 08:31:44 +0700

    :zap: add track resolver

--- odyssey-lift-off-part3/server/src/resolvers.js
+++ odyssey-lift-off-part3/server/src/resolvers.js
@@ -4,6 +4,14 @@ const resolvers = {
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
     },
+    // get a single track by id, for the Track page
+    // track: (parent, args, context, info) => {},
+    // We can replace parent parameter with an underscore.
+    // We need id of track so let's destructure it from args parameter.
+    // We need access to dataSources key so let's destructure it from context parameter.
+    track: (_, { id }, { dataSources }) => {
+      return dataSources.trackAPI.getTrack(id);
+    },
   },
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
