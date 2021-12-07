379e31b444026c1e1e168a8e5951494c857f23d9
Bramandia Gilanglaksana Adam <gilangbram@gmail.com>
2021-12-07 15:12:32 +0700

    :zap: add finished Module page

--- odyssey-lift-off-lab/client/src/pages/module.js
+++ odyssey-lift-off-lab/client/src/pages/module.js
@@ -1,12 +1,45 @@
 import React from 'react';
-import { Layout } from '../components';
+import { useQuery, gql } from '@apollo/client';
+import { Layout, QueryResult } from '../components';
+import ModuleDetail from '../components/module-detail';
+
+// GraphQL query to fetch module and parent track data (built in apollo studio)
+export const GET_MODULE_AND_PARENT_TRACK = gql`
+  query getModuleAndParentTrack($moduleId: ID!, $trackId: ID!) {
+    module(id: $moduleId) {
+      id
+      title
+      content
+      videoUrl
+    }
+    track(id: $trackId) {
+      id
+      title
+      modules {
+        id
+        title
+        length
+      }
+    }
+  }
+`;
 
 /**
  * Module page fetches both parent track data and module's detail data from the gql query GET_MODULE_AND_PARENT_TRACK
  * and provides it to the ModuleDetail component to display
  */
-const Module = () => {
-  return <Layout fullWidth></Layout>;
+const Module = ({ trackId, moduleId }) => {
+  const { loading, error, data } = useQuery(GET_MODULE_AND_PARENT_TRACK, {
+    variables: { trackId, moduleId },
+  });
+
+  return (
+    <Layout fullWidth>
+      <QueryResult error={error} loading={loading} data={data}>
+        <ModuleDetail track={data?.track} module={data?.module} />
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Module;
