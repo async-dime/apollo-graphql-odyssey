a930ec4ebd3e37993f420bb94c2124ea79764251
Bramandia Gilanglaksana Adam <gilangbram@gmail.com>
2021-12-07 15:06:38 +0700

    :zap: set up Module page and routing

---
+++ odyssey-lift-off-lab/client/src/pages/module.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Layout } from '../components';
+
+/**
+ * Module page fetches both parent track data and module's detail data from the gql query GET_MODULE_AND_PARENT_TRACK
+ * and provides it to the ModuleDetail component to display
+ */
+const Module = () => {
+  return <Layout fullWidth></Layout>;
+};
+
+export default Module;
